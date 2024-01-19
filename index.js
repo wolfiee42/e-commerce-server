2bf69d188917cbdfe1c196cb8e79d857b88dd9a3
wolfiee42 <saifalislam2022@gmail.com>
2024-01-19 21:04:27 +0600

    allproduct route created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config()
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -11,13 +12,10 @@ app.use(cors());
 app.use(express.json());
 
 
-app.get('/', (req, res) => {
-    res.send("Server is Running")
-})
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@ecommercecluster01.0tsvj1b.mongodb.net/?retryWrites=true&w=majority`;
 
 
@@ -34,10 +32,27 @@ const dbConnet = async () => {
         client.connect();
         console.log("DB Connected Successfully!");
     } catch (error) {
-        console.log(error.message);
+        console.log(error);
     }
 }
 dbConnet();
+
+const database = client.db("eCommerce")
+const productsCollection = database.collection("Products");
+
+
+app.get('/', (req, res) => {
+    res.send("Server is Running")
+})
+
+// displaying products
+app.get('/allproducts', async (req, res) => {
+    const category = req.query.category;
+    const filter = { classification: category }
+    const result = await productsCollection.find(filter).toArray();
+    res.send(result);
+})
+
 
 
 
